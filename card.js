dbf42ba4f75b9701134d266b6e930591ad823964
bulakhuseyin <huseyin.bulak@hotmail.com>
2025-04-20 02:25:57 +0300

    Delete card.js

--- card.js
+++
@@ -1,3 +0,0 @@
-
-export const Card = ({ children, ...props }) => <div className='border rounded shadow' {...props}>{children}</div>;
-export const CardContent = ({ children, ...props }) => <div {...props}>{children}</div>;
